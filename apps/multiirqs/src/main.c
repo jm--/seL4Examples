964735dbb537478f80b95117ba2dc1fd86adaadb
Josef Mihalits <jm.public@gmail.com>
2015-07-16 18:20:47 -0700

    Init commit "multiirqs" app.

---
+++ apps/multiirqs/src/main.c
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2015, Josef Mihalits
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ *
+ */
+
+#include <autoconf.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <sel4/arch/bootinfo.h>
+#include <allocman/bootstrap.h>
+#include <allocman/vka.h>
+#include <sel4platsupport/platsupport.h>
+#include <sel4platsupport/timer.h>
+#include <sel4utils/vspace.h>
+#ifdef CONFIG_KERNEL_STABLE
+#include <simple-stable/simple-stable.h>
+#else
+#include <simple-default/simple-default.h>
+#endif
+
+
+
+/* memory management: Virtual Kernel Allocator (VKA) interface and VSpace */
+static vka_t vka;
+static vspace_t vspace;
+
+/*system abstraction */
+static simple_t simple;
+
+/* root task's BootInfo */
+static seL4_BootInfo *bootinfo;
+
+/* amount of virtual memory for the allocator to use */
+#define VIRT_POOL_SIZE (BIT(seL4_PageBits) * 200)
+
+/* static memory for the allocator to bootstrap with */
+#define POOL_SIZE (BIT(seL4_PageBits) * 10)
+static char memPool[POOL_SIZE];
+
+/* for virtual memory bootstrapping */
+static sel4utils_alloc_data_t allocData;
+
+// ======================================================================
+
+/*
+ * Initialize all main data structures.
+ *
+ * The code to initialize simple, allocman, vka, and vspace is modeled
+ * after the "sel4test-driver" app:
+ * https://github.com/seL4/sel4test/blob/master/apps/sel4test-driver/src/main.c
+ */
+static void
+setup_system()
+{
+    /* initialize boot information */
+    bootinfo  = seL4_GetBootInfo();
+
+    /* initialize simple interface */
+    simple_stable_init_bootinfo(&simple, bootinfo);
+    //simple_default_init_bootinfo(simple, bootinfo);
+
+    /* create an allocator */
+    allocman_t *allocman;
+    allocman = bootstrap_use_current_simple(&simple, POOL_SIZE, memPool);
+    assert(allocman);
+
+    /* create a VKA */
+    allocman_make_vka(&vka, allocman);
+
+    /* create a vspace */
+    UNUSED int err;
+    err = sel4utils_bootstrap_vspace_with_bootinfo_leaky(&vspace,
+            &allocData, seL4_CapInitThreadPD, &vka, bootinfo);
+    assert(err == 0);
+
+    /* fill allocator with virtual memory */
+    void *vaddr;
+    UNUSED reservation_t vres;
+    vres = vspace_reserve_range(&vspace, VIRT_POOL_SIZE, seL4_AllRights,
+            1, &vaddr);
+    assert(vres.res);
+    bootstrap_configure_virtual_pool(allocman, vaddr, VIRT_POOL_SIZE,
+            seL4_CapInitThreadPD);
+}
+
+
+int main()
+{
+    setup_system();
+
+    /* enable serial driver */
+    platsupport_serial_setup_simple(NULL, &simple, &vka);
+
+    printf("\n\n>>>>>>>>>> multi-irqs <<<<<<<<<< \n\n");
+    simple_print(&simple);
+    return 0;
+}
+
